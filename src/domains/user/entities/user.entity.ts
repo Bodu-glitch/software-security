855b1c715da48ec11188cccb5c699fa9785827c5
Bodu <tn212079@gmail.com>
2025-04-12 23:33:33 +0700

    login

---
+++ src/domains/user/entities/user.entity.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({type:'text'})
+  username: string;
+
+  @Column({type:'text'})
+  password: string;
+
+  @Column({type:'text'})
+  name: string;
+
+  @Column({type:'timestamptz'})
+  createdAt: Date;
+
+}
+
